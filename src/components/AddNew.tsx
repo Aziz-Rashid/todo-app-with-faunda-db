e9472468630eae8e0ec5551077fc7bb55ce18684
aziz <axixraxhid@gmail.com>
2020-12-26 00:40:33 +0500

    update

---
+++ src/components/AddNew.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useMutation } from "@apollo/client";
+import { ADD_OPERATION, READ_OPERATION } from "../graphql/graphql";
+
+export const AddNew = () => {
+  const [addNewRecord] = useMutation(ADD_OPERATION);
+  const [todo, settodo] = React.useState("");
+
+  let addRecord = (e) => {
+    e.preventDefault();
+    try {
+      addNewRecord({
+        variables: {
+          todo: todo,
+        },
+        refetchQueries: [{ query: READ_OPERATION }],
+      });
+      settodo('')
+    } catch (err) {
+      return <div>error</div>;
+    }
+  };
+  const handleChange = (e) => {
+    settodo(e.target.value);
+  };
+  return (
+    <>
+      <form onSubmit={(e) => addRecord(e)} id="to-do-form">
+        <input value={todo} onChange={handleChange} type="text" placeholder="Enter task" />
+        <button type="submit">Add Todo</button>
+      </form>
+    </>
+  );
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Home from '../components/index'
+const index = () => {
+    return (
+        <div>
+            <Home />
+        </div>
+    )
+}
+
+export default index
